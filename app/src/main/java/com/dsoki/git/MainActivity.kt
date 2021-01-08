74cd2d39db3367ba51b6b2bfd38f38ef454e74c2
MhmdDsoki <mohammed3ala2@yahoo.com>
2021-01-08 02:07:34 +0200

    login bug fixed

---
+++ app/src/main/java/com/dsoki/git/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.dsoki.git
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //init git : initialization a git repo.
+        //Un versioned filed :we told git we want to use this files with git
+        //but we still not tracked them(mean it we want to do a backup for thee files we will tracked them
+        //but if we want to fix tis we use git + command to tell him wht we want to do
+        //git add gradlew we want to add our tracked file list to git.
+
+        //red mean un versioned green mean they are added and is versioned
+        //if you want to ignore any file which has important data you use git ignore at project  and . + file name
+        //git add gradlew 
+        //git add . : add all the files in repos and sub-directories to .
+    }
+}
